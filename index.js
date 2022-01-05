c01ec082fc318da9d65333d608c15a51a2079cc9
Chhann Chikay <51283776+chikayoo4@users.noreply.github.com>
2022-01-05 09:33:59 +0700

    add server port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send("hello world");
 });
-app.listen(4000);
+app.listen(port);
