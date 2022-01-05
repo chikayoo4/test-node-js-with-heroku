34f5ecae83d60b08c6b81455323818e7df02f778
Chhann Chikay <51283776+chikayoo4@users.noreply.github.com>
2022-01-05 09:44:41 +0700

    x

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.send("hello world!");
 });
 app.listen(port);
